93535c5503a781aeb679c1471ced95c31887269b
Godung27 <parnuvich.l@gmail.com>
2025-07-16 16:57:33 +0700

    Essentials Deep Dive_Deriving State From Props

--- 02-Essentials-Deep-Dive/src/App.jsx
+++ 02-Essentials-Deep-Dive/src/App.jsx
@@ -27,10 +27,21 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player defaultName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player defaultName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player
+            defaultName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            defaultName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
       <Log />
     </main>
--- 02-Essentials-Deep-Dive/src/components/GameBoard.jsx
+++ 02-Essentials-Deep-Dive/src/components/GameBoard.jsx
@@ -4,7 +4,16 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // const handleSelectBox = function (rowIndex, colIndex) {
@@ -23,7 +32,7 @@ export default function GameBoard({ onSelectSquare }) {
           {
             row.map((playerSymbol, colIndex) =>
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
