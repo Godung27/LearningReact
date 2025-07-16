e561d455523765ebb3cf1b16cf7f3ecf0b7dc91f
Godung27 <parnuvich.l@gmail.com>
2025-07-16 15:40:05 +0700

    Essentials Deep Dive_Best Practice: Updating Object State Immutably

--- 02-Essentials-Deep-Dive/src/components/GameBoard.jsx
+++ 02-Essentials-Deep-Dive/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,26 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectBox = function (rowIndex, colIndex) {
+    setGameBoard((preGameBoard) => {
+      const updatedBoard = [...preGameBoard.map(innerArr => [...innerArr])];
+      updatedBoard[rowIndex][colIndex] = "X"
+      return updatedBoard;
+    })
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+      {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
           {
             row.map((playerSymbol, colIndex) =>
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectBox(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             )}
         </ol>
