395f2f03d4eef24a14ae52861764243e022258b9
Godung27 <parnuvich.l@gmail.com>
2025-07-17 16:35:05 +0700

    Essentials Deep Dive_Deriving Computed Values From Other Computed Values

--- 02-Essentials-Deep-Dive/src/App.jsx
+++ 02-Essentials-Deep-Dive/src/App.jsx
@@ -37,10 +37,17 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+
   for (const combinations of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[];
-    const secondSquareSymbol
-    const thirdSquareSymbol
+    const firstSquareSymbol = gameBoard[combinations[0].row][combinations[0].column];
+    const secondSquareSymbol = gameBoard[combinations[1].row][combinations[1].column];
+    const thirdSquareSymbol = gameBoard[combinations[2].row][combinations[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      console.log(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
+      winner = firstSquareSymbol;
+    }
   }
 
   const handleSelectSquare = function (rowIndex, colIndex) {
@@ -69,6 +76,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
