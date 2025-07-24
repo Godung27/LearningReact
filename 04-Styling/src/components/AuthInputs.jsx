098e81e5a3d1d5f669bda0148dcd19bfc138cd17
Godung27 <parnuvich.l@gmail.com>
2025-07-24 19:58:55 +0700

    Styling_Styled Components: Pseudo Selectors, Nested Rules & Media Queries

--- 04-Styling/src/components/AuthInputs.jsx
+++ 04-Styling/src/components/AuthInputs.jsx
@@ -24,10 +24,24 @@ const Input = styled.input`
   line - height: 1.5;
   background-color: ${({ $checkInvalid }) => $checkInvalid ? "#fed2d2" : "#d1d5db"};
   color: ${({ $checkInvalid }) => $checkInvalid ? "#ef4444" : "#374151"};
-  border-color: ${({ $checkInvalid }) => $checkInvalid ? "#f73f3f" : "transparent"};
-  border: 1px solid transparent;
+  border: 1px solid ${({ $checkInvalid }) => $checkInvalid ? "#f73f3f" : "transparent"};
   border - radius: 0.25rem;
   box - shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+  background-color: #f0920e;
+}
 `
 
 export default function AuthInputs() {
@@ -83,7 +97,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
