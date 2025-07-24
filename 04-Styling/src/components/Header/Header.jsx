eb324adbdd7fb37b1636e178fabbd5aacb98c4d5
Godung27 <parnuvich.l@gmail.com>
2025-07-24 18:02:29 +0700

    Styling_Scoping CSS Rules with CSS Modules

--- 04-Styling/src/components/Header/Header.jsx
+++ 04-Styling/src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import logo from "../../assets/logo.png"
-import "./Header.css"
+import classes from "./Header.module.css"
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p
+      <p className={1 === 1 ? `${classes.paragraph}` : undefined}
       // style={{
       //   color: "green",
       //   // "margin- left": 100, // same
       //   marginLeft: 100,
       // }}
-      >A community of artists and art-lovers.</p>
-    </header>
+      > A community of artists and art-lovers.</p>
+    </header >
   );
 }
