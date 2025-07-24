d49f7fffd072498ab8af40010f50214bf2b3934e
Godung27 <parnuvich.l@gmail.com>
2025-07-24 23:11:39 +0700

    Styling_Creating Reusable Components & Component Combinations

--- 04-Styling/src/components/AuthInputs.jsx
+++ 04-Styling/src/components/AuthInputs.jsx
@@ -1,47 +1,14 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { styled } from "styled-components";
+
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $checkInvalid }) => $checkInvalid ? "#f87171" : "#6b7280"};
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line - height: 1.5;
-  background-color: ${({ $checkInvalid }) => $checkInvalid ? "#fed2d2" : "#d1d5db"};
-  color: ${({ $checkInvalid }) => $checkInvalid ? "#ef4444" : "#374151"};
-  border: 1px solid ${({ $checkInvalid }) => $checkInvalid ? "#f73f3f" : "transparent"};
-  border - radius: 0.25rem;
-  box - shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-  background-color: #f0920e;
-}
 `
 
 export default function AuthInputs() {
@@ -67,31 +34,35 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p className="paragraph">
-          <Label $checkInvalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $checkInvalid={emailNotValid}
-            // style={{
-            //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db"
-            // }}
-            // className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
+        {/* <p className="paragraph">
+        <Label $checkInvalid={emailNotValid}>Email</Label> */}
+        <Input
+          label="Email"
+          type="email"
+          checkInvalid={emailNotValid}
+          // style={{
+          //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db"
+          // }}
+          // className={emailNotValid ? 'invalid' : undefined}
+          onChange={(event) =>
+            handleInputChange('email', event.target.value)
+          }
+        />
+        {/* </p>
         <p>
-          <Label $checkInvalid={passwordNotValid}
+        <Label $checkInvalid={passwordNotValid}
           // className={`label ${emailNotValid ? "invalid" : ""} `}
-          >Password</Label>
-          <Input
-            $checkInvalid={passwordNotValid}
-            type="password"
-            // className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+          >Password</Label> */}
+        <Input
+          label="Password"
+          type="password"
+          checkInvalid={passwordNotValid}
+          // className={passwordNotValid ? 'invalid' : undefined}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
+        {/* </p> */}
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
---
+++ 04-Styling/src/components/Input.jsx
@@ -0,0 +1,31 @@
+import { styled } from "styled-components"
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ $checkInvalid }) => $checkInvalid ? "#f87171" : "#6b7280"};
+`
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line - height: 1.5;
+  background-color: ${({ $checkInvalid }) => $checkInvalid ? "#fed2d2" : "#d1d5db"};
+  color: ${({ $checkInvalid }) => $checkInvalid ? "#ef4444" : "#374151"};
+  border: 1px solid ${({ $checkInvalid }) => $checkInvalid ? "#f73f3f" : "transparent"};
+  border - radius: 0.25rem;
+  box - shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
+export default function CustomInput({ label, checkInvalid, ...props }) {
+  return (
+    <p>
+      <Label $checkInvalid={checkInvalid}>{label}</Label>
+      <Input $checkInvalid={checkInvalid} {...props} />
+    </p>
+  );
+}
