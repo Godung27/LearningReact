346bd0fa3e78266b82a1607a702c8624f891069b
Godung27 <parnuvich.l@gmail.com>
2025-07-28 15:02:46 +0700

    Refs Portals_Introducing Refs: Connecting & Accessing HTML Elements via Refs

--- 06-Refs-Portals/src/components/Player.jsx
+++ 06-Refs-Portals/src/components/Player.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const playerName = useRef();
 
-  const handleChangeName = function (e) {
-    setSubmitted(false)
-    setEnteredPlayerName(e.target.value)
-  }
+  // const [submitted, setSubmitted] = useState(false);
+
+  // const handleChangeName = function (e) {
+  //   setSubmitted(false)
+  //   setEnteredPlayerName(e.target.value)
+  // }
 
   const handleClick = function () {
-    setSubmitted(true)
+    setEnteredPlayerName(playerName.current.value);
+    // setSubmitted(true)
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChangeName} value={enteredPlayerName} />
+        <input
+          ref={playerName}
+          type="text"
+        // onChange={handleChangeName} 
+        // value={enteredPlayerName}   
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
