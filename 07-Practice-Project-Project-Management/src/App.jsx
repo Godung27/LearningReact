372093642d4a8036c8ca7e3f8411b570338d4d77
Godung27 <parnuvich.l@gmail.com>
2025-08-04 18:23:43 +0700

    Practice Project-Project Management_Managing State to Switch Between Components

--- 07-Practice-Project-Project-Management/src/App.jsx
+++ 07-Practice-Project-Project-Management/src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 import Sidebar from "./components/Sidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectProjectId: undefined,
+    project: [],
+  });
+
+  const handleStartAddProject = function () {
+    setProjectsState(preState => {
+      return {
+        ...preState,
+        selectProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
