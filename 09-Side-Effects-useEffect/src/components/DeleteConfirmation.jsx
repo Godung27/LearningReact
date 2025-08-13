25a29c56c5c306309866c057b33c2642ce1b4d6f
Godung27 <parnuvich.l@gmail.com>
2025-08-13 16:07:06 +0700

    Side Effects useEffect_The Problem with Object & Function Dependencies

--- 09-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
+++ 09-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
@@ -2,14 +2,16 @@ import { useEffect } from "react";
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
+    console.log("first")
     const timer = setTimeout(() => {
       onConfirm();
     }, 3000);
 
     return () => {
+      console.log("clear")
       clearTimeout(timer);
     }
-  }, [])
+  }, [onConfirm])
 
   return (
     <div id="delete-confirmation">
