bdc4b2823897664df8144cdcd4a8b06df1a4f54a
Godung27 <parnuvich.l@gmail.com>
2025-08-13 16:18:02 +0700

    Side Effects useEffect_The useCallback Hook

--- 09-Side-Effects-useEffect/src/App.jsx
+++ 09-Side-Effects-useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -53,7 +53,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -61,7 +61,7 @@ function App() {
 
     const storedIds = JSON.parse(localStorage.getItem("selectedPlace")) || [];
     localStorage.setItem("selectedPlace", JSON.stringify(storedIds.filter(id => id !== selectedPlace.current)))
-  }
+  }, []);
 
   return (
     <>
