07352e4c170f7bedd2591bcbc309e761656a2837
Godung27 <parnuvich.l@gmail.com>
2025-08-14 19:21:45 +0700

    Demo Project React Quiz App_Deriving Values, Outputting Questions & Registering Answers

--- 10-Demo-Project-React-Quiz-App/src/components/Header.jsx
+++ 10-Demo-Project-React-Quiz-App/src/components/Header.jsx
@@ -2,7 +2,7 @@ import quizLogoImg from "../assets/quiz-logo.png"
 
 export default function Header() {
   return (
-    <header className="header">
+    <header>
       <img src={quizLogoImg} alt="Quiz logo" />
       <h1>ReactQuiz</h1>
     </header>
