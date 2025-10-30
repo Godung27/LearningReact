3e0a302cf7f01df4074d3da5d86e2d9fccc3f4fa
Godung27 <parnuvich.l@gmail.com>
2025-10-31 00:00:32 +0700

    HTTP Requests_Transforming Fetched Data

--- 13-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 13-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from "../loc.js"
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,19 +14,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/placesssssssssss");
+        const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
 
         if (!response.ok) {
           throw new Error("Failed")
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
       } catch (error) {
         setError({ message: error.message || "Not fetch places" });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
