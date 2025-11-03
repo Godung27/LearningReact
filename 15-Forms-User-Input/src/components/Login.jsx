e4f746f9baf5d09bcd3082d467e809bed4aa5ec1
Godung27 <parnuvich.l@gmail.com>
2025-11-03 17:22:23 +0700

    Forms User Inputs_Managing & Getting User Input via State & Generic Handlers

--- 15-Forms-User-Input/src/components/Login.jsx
+++ 15-Forms-User-Input/src/components/Login.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   const handleSubmit = function (e) {
     e.preventDefault();
-    console.log("kk")
+
+    console.log(enteredValues);
   }
+
+  const handleInputChange = function (identifier, value) {
+    setEnteredValues(preValues => ({
+      ...preValues,
+      [identifier]: value,
+    }));
+  }
+
+  // const handleChangeEmail = function (e) {
+  //   setEnteredEmail(e.target.value);
+  // }
+
+  // const handleChangePassword = function (e) {
+  //   setEnteredPassword(e.target.value);
+  // }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -10,12 +36,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(e) => handleInputChange("email", e.target.value)} value={enteredValues.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(e) => handleInputChange("password", e.target.value)} value={enteredValues.password} />
         </div>
       </div>
 
