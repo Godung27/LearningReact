932cb7cfa2cf18d50bdf8c1cf0fd9e0c2ef770b9
Godung27 <parnuvich.l@gmail.com>
2025-11-06 23:52:49 +0700

    Forms User Input_Validating Input Upon Form Submission

--- 15-Forms-User-Input/src/components/Login.jsx
+++ 15-Forms-User-Input/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -11,13 +13,17 @@ export default function Login() {
     const enteredPassword = password.current.value;
 
     console.log(enteredEmail, enteredPassword);
-  }
 
-  const handleInputChange = function (identifier, value) {
-    setEnteredValues(preValues => ({
-      ...preValues,
-      [identifier]: value,
-    }));
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("...")
   }
 
   return (
@@ -28,6 +34,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
