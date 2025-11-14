c9f207b3538a93ef1965f60afd5a556bdd052c4e
Godung27 <parnuvich.l@gmail.com>
2025-11-14 17:33:11 +0700

    Practice Project-Food Order_Finishing & Using the Cart Context & Reducer

--- 17-Practice-Project-Food-Order/src/store/CartContext.jsx
+++ 17-Practice-Project-Food-Order/src/store/CartContext.jsx
@@ -30,18 +30,49 @@ const cartReducer = function (state, action) {
   }
 
   if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
 
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export const CartContextProvider = function ({ children }) {
-  useReducer();
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: 'ADD_ITEM', item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem
+  };
 
   return (
-    <CartContext>{children}</CartContext>
+    <CartContext value={cartContext}>{children}</CartContext>
   );
 }
 
-export default CartContext
+export default CartContext;
