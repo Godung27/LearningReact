c47013e69a92d9ec783f518370ee1b11399117bc
Godung27 <parnuvich.l@gmail.com>
2025-11-16 21:33:36 +0700

    Practice Project-Food Order_Adding a Reusable Modal Component with useEffect

---
+++ 17-Practice-Project-Food-Order/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ open, className = "", children }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+  }, [open]);
+
+  return (
+    createPortal(
+      <dialog
+        ref={dialog}
+        className={`modal ${className}`}
+      >
+        {children}
+      </dialog>,
+      document.getElementById("modal")
+    );
+  );
+}
