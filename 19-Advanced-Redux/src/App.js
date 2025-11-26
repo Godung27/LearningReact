8948f4b40aa4fd117819ab93d9cd455f2a0f13eb
Godung27 <parnuvich.l@gmail.com>
2025-11-26 15:52:11 +0700

    Advanced Redux_Using an Action Creator Thunk

--- 19-Advanced-Redux/src/App.js
+++ 19-Advanced-Redux/src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart.js';
 import Layout from './components/Layout/Layout.js';
 import Products from './components/Shop/Products.js';
 import Notification from "./components/UI/Notification.js";
-import { uiActions } from './store/uiSlice.js';
+import { sendCartData } from './store/cartSlice.js';
 
 let isInitial = true;
 
@@ -15,52 +15,13 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
-
   useEffect(() => {
-    const sendCartData = async function () {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-
-      const response = await fetch(
-        "https://godung-f6030-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
-
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
