3044cee1fa2beb139b29538dd2f26a088970ff0d
Godung27 <parnuvich.l@gmail.com>
2025-11-26 17:36:53 +0700

    Advanced Redux_Getting Started with Fetching Data

---
+++ 19-Advanced-Redux/src/store/cartAction.js
@@ -0,0 +1,81 @@
+import { uiActions } from './uiSlice';
+import { cartActions } from './cartSlice';
+
+export const fetchCartData = function () {
+  return async function (dispatch) {
+    const fetchData = async function () {
+      const response = await fetch(
+        "https://godung-f6030-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+
+  };
+};
+
+export const sendCartData = function (cart) {
+  return async function (dispatch) {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async function () {
+      const response = await fetch(
+        "https://godung-f6030-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
