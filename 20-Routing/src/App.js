22658272f1d7b75ac62bd4294b7a8ae39493b8ab
Godung27 <parnuvich.l@gmail.com>
2025-11-28 15:51:45 +0700

    Routing_Navigating between Pages with Links

--- 20-Routing/src/App.js
+++ 20-Routing/src/App.js
@@ -1,22 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { createRoutesFromElements, Route } from "react-router-dom";
+// import { createRoutesFromElements, Route } from "react-router-dom";
 
 import Home from "./pages/Home.js";
 import Products from "./pages/Products.js";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
+// Old Version
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
-const router = createBrowserRouter(routeDefinitions);
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Products /> },
-// ])
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+])
 
 function App() {
   return <RouterProvider router={router} />;
--- 20-Routing/src/pages/Home.js
+++ 20-Routing/src/pages/Home.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <p>My Home Page</p>
+    <>
+      <p>My Home Page</p>
+      <p>
+        Go to <Link to={"/products"}>List of Products</Link>
+      </p>
+    </>
   );
 }
