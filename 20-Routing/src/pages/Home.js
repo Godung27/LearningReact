09aef71ef5ef73bf3ae2acffcca2e1dc6aa46168
Godung27 <parnuvich.l@gmail.com>
2025-11-29 15:21:02 +0700

    Routing_Navigating Programmatically

--- 20-Routing/src/pages/Home.js
+++ 20-Routing/src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleNavigate = function () {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to={"/products"}>List of Products</Link>
+      </p>
+      <p>
+        <button onClick={handleNavigate} >Navigate</button>
       </p>
     </>
   );
