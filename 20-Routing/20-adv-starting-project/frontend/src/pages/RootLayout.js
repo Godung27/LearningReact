4801b4e60b88515ac761452215c36200a3671811
Godung27 <parnuvich.l@gmail.com>
2025-12-08 17:19:58 +0700

    Routing_Reflecting The Current Navigation State in the UI

--- 20-Routing/20-adv-starting-project/frontend/src/pages/RootLayout.js
+++ 20-Routing/20-adv-starting-project/frontend/src/pages/RootLayout.js
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 export default function RootLayout() {
+  // const navitation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navitation.state === "loading" && <p>loading...</p>} */}
         <Outlet />
       </main>
     </>
